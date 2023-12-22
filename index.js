a2895ed3f9a63209bdb4b2217d9238de82cf6575
Glitch (nine-mirror-bead) <none>
2023-12-22 03:52:34 +0000

    🧹💿 Updated with Glitch

--- index.js
+++ index.js
@@ -28,3 +28,13 @@ app.get('/api/hello', function (req, res) {
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+let responseObject = {}
+app.enable('trust proxy')
+app.get('/api/whoami', (req, res) => {
+  responseObject['ipaddress'] = req.ip
+  responseObject['language'] = req.get('Accept-Language')
+  responseObject['software'] = req.get('User-Agent')
+  
+  res.json(responseObject)
+})
